7e08a9d4341533dbc3dbb78c45068cc2015c0368
John Smith <awesomebing1@gmail.com>
2015-05-29 10:01:16 -0500

    We're switching our account.

--- js/script.js
+++ js/script.js
@@ -42,9 +42,9 @@ function container(type) {
             $(".container").text("I'm loading right now. If I stay here like this, something's broken.");
             $(".container").load("views/calendar.html", function() {
                 $('#calendar').fullCalendar({
-                    googleCalendarApiKey: window.atob("QUl6YVN5Q1NqMkNnbXFlaGZKQ0ZQM1JsUFlZSHVFZ0JoWmJmb2ZR"),
+                    googleCalendarApiKey: window.atob("QUl6YVN5QXNhX3U1SnBBN0xoSnY0NGdwWWpVRDZST2FoUW83eEhJ"),
                     events: {
-                        googleCalendarId: window.atob("aWN1dHY3anAwMmt1dXNlc2c2bThiazA4NHNuZWkxMjJAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20=")
+                        googleCalendarId: window.atob("cHRhb2ZmbXNAZ21haWwuY29t")
                     }
                 });
             });
